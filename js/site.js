57bda8b51ec921c061269594f3e06a8fa4a8f002
FiniteLooper <463685+FiniteLooper@users.noreply.github.com>
2024-01-03 15:06:33 +0000

    Deploying to gh-pages from @ FiniteLooper/debbiebarr.com@475f928d4bcc0ccb1ea5571074cbe05ec877cd13 🚀

---
+++ js/site.js
@@ -0,0 +1,59 @@
+(() => {
+  const $body = document.querySelector('body');
+  const $toggleSidebar = document.querySelector('#toggle-sidebar');
+  const $sidebar = document.querySelector('#sidebar');
+  const $closeSidebar = document.querySelector('#close-sidebar');
+  const $sidebarLinks = Array.from(document.querySelectorAll('#side-navigation a'));
+  const $sidebarBackdrop = document.querySelector('#sidebar-backdrop');
+
+  //--------------------------------------------
+  //SIDE NAVIGATION
+  //--------------------------------------------
+  //make the side nav links not focusable by default
+  $sidebarLinks.forEach((l) => (l.tabIndex = -1));
+
+  let sideNavOpen = false;
+  function updateSidebarClass() {
+    $body.classList.toggle('sidebar-open', sideNavOpen);
+
+    if (sideNavOpen) {
+      $sidebar.focus();
+    } else {
+      document.activeElement.blur();
+    }
+
+    //make the links focusable only when shown on screen
+    $sidebarLinks.forEach((l) => (l.tabIndex = sideNavOpen ? 0 : -1));
+  }
+
+  function closeSidebar(ev) {
+    ev.preventDefault();
+    sideNavOpen = false;
+    updateSidebarClass();
+  }
+
+  document.body.addEventListener(
+    'keyup',
+    (ev) => {
+      if (sideNavOpen && ev.key === 'Escape') {
+        sideNavOpen = false;
+        updateSidebarClass();
+      }
+    },
+    false,
+  );
+
+  $toggleSidebar.addEventListener(
+    'click',
+    (ev) => {
+      ev.preventDefault();
+      sideNavOpen = !sideNavOpen;
+      updateSidebarClass();
+    },
+    false,
+  );
+
+  $closeSidebar.addEventListener('click', closeSidebar, false);
+  $sidebarBackdrop.addEventListener('click', closeSidebar, false);
+  $sidebarBackdrop.addEventListener('touchstart', closeSidebar, false);
+})();
