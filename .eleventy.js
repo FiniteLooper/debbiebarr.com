97583f07a523c347f2cf4f640df02cd127f5ae0f
Chris Barr <chris.m.barr@gmail.com>
2024-05-26 16:13:50 -0400

    compress script tags too

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,7 @@ module.exports = (eleventyConfig) => {
         collapseWhitespace: true,
         removeComments: true,
         useShortDoctype: true,
+        minifyJS: true,
       });
     }
 
